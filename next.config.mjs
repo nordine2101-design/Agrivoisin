122734d1a4894f3609208c2c0d2a70519305b728
nordine2101-design <nordine2101@gmail.com>
2026-08-16 19:09:41 +0200

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: false, // Désactive l'optimisation lourde pour débloquer le serveur immédiatement
+};
+
+export default nextConfig;

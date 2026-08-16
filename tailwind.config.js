dbe2b691fec5865510d603a005d651093b69d29e
nordine2101-design <nordine2101@gmail.com>
2026-08-16 17:45:41 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        agriVert: '#76c823',
+      },
+    },
+  },
+  plugins: [],
+}

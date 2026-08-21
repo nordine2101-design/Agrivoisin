86d73bedbce9a3ae9465003906ba7b2b17ddeb59
nordine2101-design <nordine2101@gmail.com>
2026-08-21 16:07:12 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // Empêche Vercel de boucler à l'infini sur le build
-  swcMinify: false,
   typescript: {
     ignoreBuildErrors: true,
   },
